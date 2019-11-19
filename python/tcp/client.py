bd3bab1bc0c99cf40f49c5dad2a09b2b36109a09
Fabian Melendez <fabian.melendez.a@gmail.com>
2019-11-19 13:37:24 -0600

    Timeout added to TCP client

--- python/tcp/client.py
+++ python/tcp/client.py
@@ -5,14 +5,25 @@ import socket
 import atexit
 import common
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# Define the address family (IPv4) and the data transfer protocol (TCP)
-s.connect((common.HOST, common.PORT))		# three-way handshake
-atexit.register(common.on_quit, s) 	# registers callback to exit signal
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	# define the address family (IPv4) and the data transfer protocol (TCP)
+try:
+	s.connect((common.HOST, common.PORT))				# three-way handshake
+except:
+	print("\nCan't connect to server.\n")
+	exit()
+s.settimeout(1)											# set a timeout in case the server doesn't respond
+atexit.register(common.on_quit, s) 						# register callback to exit signal
+
 while 1:
 	print(">> ", end= "")
 	msg = raw_input()
-	s.sendall(msg)			# send input text
-	data = s.recv(1024)		# sync wait for a response from the server
-	print('Received', repr(data), "\n")
+	if msg == '': continue
+	try:
+		s.sendall(msg)				# send input text
+		data = s.recv(1024)		# sync wait for a response from the server
+		print('Received', repr(data), "\n")
+	except:
+		print('\nThe socket timed out. No response from the server.\n')
+		exit()
 
 
--- python/tcp/common.py
+++ python/tcp/common.py
@@ -1,5 +1,5 @@
 HOST = '127.0.0.1'    	# remote host
-PORT = 42501            # port used by both server and client
+PORT = 42500            # port used by both server and client
 
 def on_quit(s, addr=(HOST, PORT)):
 	"""Common handler for closing sockets.
